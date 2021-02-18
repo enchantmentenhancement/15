7aa44f14833d2b8da01d614aea05d2e349a59abc
Fluff5 <76451731+Fluff5@users.noreply.github.com>
2021-02-18 09:15:34 +0000

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -131,7 +131,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "Alive!" : "Dead!";
+  var message = won ? "You win!" : "Game over!";
 
   if (typeof ga !== "undefined") {
     ga("send", "event", "game", "end", type, this.score);
